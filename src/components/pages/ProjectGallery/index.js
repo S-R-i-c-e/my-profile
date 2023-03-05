1eb040d3979e33b58f3617ba791815cd5c0ffbdc
Stephen Rice <goatwipe@gmail.com>
2023-03-05 16:33:59 +0000

    project carousel outline

--- src/components/pages/ProjectGallery/index.js
+++ src/components/pages/ProjectGallery/index.js
@@ -1,16 +1,48 @@
 import React from "react";
+import styles from "./my-style.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProjectGallery() {
+  const carouselItems = [(<Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/500/300?img=1"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>),
+  (<Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://picsum.photos/500/300?img=3"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>)];
   return (
     <div>
-      <h1>Project Gallery</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <div className='container-fluid' >
+        <div className="row">
+          <div className="col-sm-12">
+            <h3>React Bootstrap Carousel</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <Carousel>
+              {carouselItems}
+
+            </Carousel>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
