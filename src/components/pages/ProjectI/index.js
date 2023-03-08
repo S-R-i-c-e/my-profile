424c7b9a0ab30b40357b9f955160c70346013699
Stephen Rice <goatwipe@gmail.com>
2023-03-08 13:27:21 +0000

    css comes and goes

--- src/components/pages/ProjectI/index.js
+++ src/components/pages/ProjectI/index.js
@@ -8,12 +8,11 @@ function ProjectI(data) {
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={data.image} />
         <Card.Body>
-          <Card.Title>{data.heading}</Card.Title>
+          <Card.Title>{data.summary}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            {data.description}
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button href={data.repo} variant="primary">{data.heading}</Button>
         </Card.Body>
       </Card>
     );
