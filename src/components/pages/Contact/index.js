f7da8a00d4b4891d49e49fa961b07e02dbf5f86f
Stephen Rice <goatwipe@gmail.com>
2023-03-08 14:00:03 +0000

    null message

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import Learn from '../Learn';
-
 
 function Contact(props) {
   return (
     <div>
-      <style type="text/css">
-        {`
-        body {
-          background-color: var(--back-ground-color);
-        }
-        h1 {
-          
-        }
-        `}
-      </style>
       <h1>Contact Page</h1>
       <p>
         
       </p>
-
     </div>
   );
 }
